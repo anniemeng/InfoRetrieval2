69767d4f3b2fc036769480587e1c3cb1fc049ffe
anniemeng <anniezmeng@gmail.com>
2015-12-01 04:05:42 +0100

    Modified main to parse queries, and do a two pass document scan for language based model parameters. Also added higher level documentation outlining the current implementation

--- SearchEngine/src/main/main.scala
+++ SearchEngine/src/main/main.scala
@@ -1,99 +1,176 @@
 package main
 
+import java.io.{FileInputStream, BufferedInputStream}
+
 import Parsing._
 import LanguageBasedModel._
 import TermBasedModel._
 
+import collection.immutable.Map
+import collection.mutable.{Map=>MutMap}
+import collection.mutable.LinkedHashMap
+import collection.mutable.MutableList
+import scala.collection.mutable
+
 object main {
   def main(args: Array[String]) {
-    val numResults = 100
+
+    // TODO: let user specify how many n best queries they want
 
     // 1) GATHERING QUERIES
-    // For testing - comment out
-    val query = "Airbus Subsidies"
-    val queries = new Array[String](1)
-    queries(0) = query
+    val topicStream = DocStream.getStream("Tipster/topics_small_57")
+    val topics = scala.io.Source.fromInputStream(topicStream).mkString.split("</top>")
 
-    // For actual command line - uncomment
-    // val queries = args
+    val topicNumToTitle = mutable.LinkedHashMap[String, String]() // preserve order
+    var queryTerms = Set[String]()
+    for (topicBody <- topics) {
+      val numParts = topicBody.split("Number:")
+      val topicParts = topicBody.split("Topic:")
 
-    // 2) STORE A MODEL FOR EACH QUERY
-    // might want to change to dictionary mapping query -> model
-    val alertsTermQueries = new Array[TermBasedModel.AlertsTipster](queries.length)
-    val alertsLangQueries = new Array[LanguageBasedModel.AlertsTipster](queries.length)
-    var index = 0
-    for (query <- queries) {
-      val alertsTerm = new TermBasedModel.AlertsTipster(query, numResults)
-      val alertsLanguage = new LanguageBasedModel.AlertsTipster(query, numResults)
-      alertsTermQueries(index) = alertsTerm
-      alertsLangQueries(index) = alertsLanguage
-      index += 1
+      if (numParts.length > 1 && topicParts.length > 1) {
+        // get topic number and query
+        val num = numParts(1).split("\n")(0).trim()
+        val title = topicParts(1).split("\n")(0).trim()
+        topicNumToTitle(num) = title
+
+        // collect query terms (individual words in each query) for general access later on
+        val terms = title.split(" ")
+        for (term <- terms) {
+          queryTerms += term.replaceAll("[^A-Za-z0-9]", "")
+        }
+      }
     }
 
-    // 3) PROCESS DOCUMENT STREAM
-    val tipster = new TipsterStream("Tipster/zips")
+    /************* TESTING 1) *****************/
+    /*
+    println("topic num to title map:")
+    for ((num, topic) <- topicNumToTitle) {
+      println(num)
+      println(topic)
+    }
+
+    println("num query terms: " + queryTerms.size)
+    println("all query terms:")
+    for (term <- queryTerms) {
+      println(term)
+    }
+    return
+    */
+
+    // 2) PROCESS DOCUMENT STREAM
+    val tipster = new TipsterStream("Tipster/zips-1")
     println("Number of files in zips = " + tipster.length)
 
     val sw = new StopWatch
     sw.start
-    for (doc <- tipster.stream) {
-      // process document for term based model
-      for (alertsTerm <- alertsTermQueries) {
-        alertsTerm.process(doc.name, doc.tokens)
-      }
 
-      // process document for lang based model
-      for (alertsLanguage <- alertsLangQueries) {
-        alertsLanguage.process(doc.name, doc.tokens)
+    /* FIRST PASS
+     * 1) Get total number of words in general
+     * 2) Get query term frequency for all documents in the collection
+     */
+    var numWordsCollection = 0
+    val queryTermsFreqTotal = mutable.LinkedHashMap[String, Int]() // query terms freq for WHOLE document collection
+    for (doc <- tipster.stream) {
+      // total number of words in collection
+      numWordsCollection += doc.tokens.length
+
+      // query term frequency
+      // frequency of all tokens in document
+      val tfs : Map[String,Int]= doc.tokens.groupBy(identity).mapValues(l => l.length)
+
+      // filter map of all tokens to only those in query
+      val qtfs = tfs.filterKeys(queryTerms)
+      for ((queryTerm, freq) <- qtfs) {
+        val count = queryTermsFreqTotal.getOrElse(queryTerm, 0)
+        queryTermsFreqTotal(queryTerm) = count + freq
       }
     }
 
     sw.stop
     println("Stopped time = " + sw.stopped)
-//    println("Term:")
-//    alertsTerm.results.take(5).foreach(println)
-//    println("Lang:")
-//    alertsLanguage.results.take(5).foreach(println)
+    /************* TESTING FIRST PASS *****************/
+    /*
+    println("total num words:" + numWordsCollection)
+    for ((term, freq) <- queryTermsFreqTotal) {
+      println(term + ": " + freq)
+    }
+    return
+    */
 
-    // 4) PRINTING RELEVANCE FOR EACH QUERY
-    // For command line purposes:
-    var topic = 51
-    var indexAlert = 0
-    while(indexAlert < queries.length) {
-      val rel = new TipsterGroundTruth("Tipster/qrels").judgements.get(topic.toString()).get.toSet
-      val retTerm = alertsTermQueries(indexAlert).results.map(r => r.title)
-      val prTerm = new PrecisionRecall(retTerm, rel)
-      println(alertsTermQueries(indexAlert).query) // print query
-      println("Term:")
-      println(prTerm.relevIdx.mkString(" "))
-      println(prTerm.precs.mkString(" "))
-      println(prTerm.iprecs.mkString(" "))
+    /* SECOND PASS
+     * 1) Find word count of document
+     * 2) Get frequency of each query term in the document for all queries
+     * 3) Call models
+     */
+    val sw2 = new StopWatch
+    sw2.start
 
-      val retLang = alertsLangQueries(indexAlert).results.map(r => r.title)
-      val prLang = new PrecisionRecall(retLang, rel)
-      println("Lang:")
-      println(prLang.relevIdx.mkString(" "))
-      println(prLang.precs.mkString(" "))
-      println(prLang.iprecs.mkString(" "))
+    /************* TESTING SECOND PASS *****************/
+    // check if it matches total counts
+    //val testQueryTerms = mutable.LinkedHashMap[String, Int]()
 
-      topic += 1
-      indexAlert += 1
+    for (doc <- tipster.stream) {
+      // get word count of document
+      val numWordsDoc = doc.tokens.length
+
+      // get frequency of each term for query terms
+      // for each query, get a word in the query -> frequency
+      val queryTermsToFreq = mutable.LinkedHashMap[String, Map[String, Int]]()
+
+      // frequency of all tokens in document
+      val tfs : Map[String,Int]= doc.tokens.groupBy(identity).mapValues(l => l.length)
+
+      // filter map of all tokens to only those in query
+      for ((_, topic) <- topicNumToTitle) {
+        val qterms = topic.split(" ")
+        val qtfs = tfs.filterKeys(qterms.toSet)
+        queryTermsToFreq(topic) = qtfs
+      }
+
+      val docId = doc.name
+      // PASS TO MODEL
+      // languageModel(queryTermsToFreq, docId, queryTermsFreqTotal, numWordsDoc, numWordsCollection)
+
+      /************* TESTING SECOND PASS *****************/
+      /*
+      println("num words in doc: " + numWordsDoc)
+      println("doc id: " + docId)
+      for ((_, map) <- queryTermsToFreq) {
+        for ((term, freq) <- map) {
+          val count = testQueryTerms.getOrElse(term, 0)
+          testQueryTerms(term) = count + freq
+        }
+      }
+      */
     }
 
-    // testing purposes - COMMENT OUT
-    val rel = new TipsterGroundTruth("Tipster/qrels").judgements.get("51").get.toSet
-    val retTerm = alertsTermQueries(0).results.map(r => r.title)
-    val retLang = alertsLangQueries(0).results.map(r => r.title)
-    val prTerm = new PrecisionRecall(retTerm, rel)
-    println("Term:")
-    println(prTerm.relevIdx.mkString(" "))
-    println(prTerm.precs.mkString(" "))
-    println(prTerm.iprecs.mkString(" "))
+    sw2.stop
+    println("Stopped time = " + sw2.stopped)
+    /************* TESTING SECOND PASS *****************/
+    /*
+    for ((term, freq) <- testQueryTerms) {
+      println(term + ": " + freq)
+    }
+    return
+    */
 
-    val prLang = new PrecisionRecall(retLang, rel)
-    println("Lang:")
-    println(prLang.relevIdx.mkString(" "))
-    println(prLang.precs.mkString(" "))
-    println(prLang.iprecs.mkString(" "))
-}
+    // TODO: output results to a file - esp when given final 10 test queries
+
+
+    // TODO: relevance - P, R, F1, MAP
+//    val rel = new TipsterGroundTruth("Tipster/qrels").judgements.get("51").get.toSet
+//    val retTerm = alertsTermQueries(0).results.map(r => r.title)
+//    val retLang = alertsLangQueries(0).results.map(r => r.title)
+//    val prTerm = new PrecisionRecall(retTerm, rel)
+//    println("Term:")
+//    println(prTerm.relevIdx.mkString(" "))
+//    println(prTerm.precs.mkString(" "))
+//    println(prTerm.iprecs.mkString(" "))
+//
+//    val prLang = new PrecisionRecall(retLang, rel)
+//    println("Lang:")
+//    println(prLang.relevIdx.mkString(" "))
+//    println(prLang.precs.mkString(" "))
+//    println(prLang.iprecs.mkString(" "))
+  }
 }
