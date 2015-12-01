d881000ec4b191ec2ec22003b3ea3ce7af022042
Ingo <ingo.schilken@gmail.com>
2015-12-01 17:33:26 +0100

    Added heap to TF and TFIDF scoring

--- SearchEngine/src/TermBasedModel/TFScore.scala
+++ SearchEngine/src/TermBasedModel/TFScore.scala
@@ -1,12 +1,31 @@
 package TermBasedModel
 
+import main._
+
 import collection.mutable.LinkedHashMap
 import scala.collection.mutable
 
 class TFScore {
 
-  def score(tfs: mutable.LinkedHashMap[String, Map[String, Int]]): LinkedHashMap[String, Int] = {
-    return tfs.map{ case (q, m) => (q, m.foldLeft(0)(_+_._2)) }
+  def score(tfs: mutable.LinkedHashMap[String, Map[String, Int]], doc_id: String) {
+    val scores = tfs.map{ case (q, m) => (q, m.foldLeft(0)(_+_._2)) }
+    
+    var counter = 0
+    for ((q, score) <- scores) {
+      // update the min heap
+      val heap_size = main.minHeapsTerm(counter).size
+      if (heap_size < 100) {
+        main.minHeapsTerm(counter) += Tuple2(score, doc_id)
+      } else {
+        val testScore = main.minHeapsTerm(counter).head._1
+        if (score > testScore) {
+          main.minHeapsTerm(counter) += Tuple2(score,doc_id)
+          main.minHeapsTerm(counter).dequeue()
+        }
+      }
+      counter += 1
+    }
+    
   }
   
 }
